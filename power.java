a4069309700d8155e7565ec1df96c507112b9efa
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-22 16:19:46 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,21 @@
+import java.Scanner.util.*;
+public class palindrome
+{
+public static void main(String[]args)
+{
+int r,temp, n,sum=0;
+Scanner sc= new Scanner(System.in);
+n=sc.nextInt();
+while(n>o)
+{
+r=n%10;
+sum=(sum*10)+r;
+}
+if(temp==sum)
+{
+System.out.println("palindrom");
+else
+System.out.println("not palindrom");
+}
+System.out.println(n);
+}
