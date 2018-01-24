dfbd380099d131974bffbd88cb9aa3ce243adbe4
hemahemasri <30549523+hemahemasri@users.noreply.github.com>
2018-01-24 16:01:50 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,20 @@
+import java.Scanner.util.*.;
+public class odd
+{
+public static void main(String[]args)
+{
+int 
+Scanner sc=new Scanner(System.in);
+o=sc.nextInt();
+Syatem.out.println("enter the first interval:");
+for(int i=1;i<=5;i++)
+{
+if(i%2==1)
+{
+System.out.println(i);
+i++;
+}
+}
+system.out.println(o);
+}
+}
